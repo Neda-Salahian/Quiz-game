7f96c5c598ad1afb10924d4aa58a9976a31ff266
Neda Salahian <neda.salahian@gmail.com>
2023-10-24 15:25:17 +0200

    add a function for random of question.json

--- index.js
+++ index.js
@@ -4,26 +4,47 @@ import advancedquestions from './data/advancedquestions.json' assert { type: "js
 // Resource: https://v8.dev/features/import-assertions
 // Tip: assert { type: "json" } 
 
+
+
+function randomQuestion(array) {
+  // Resource: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  // shuffle
+  // Create a copy of the array to avoid modifying the original array
+  const randomQuestionArray = [...array];
+  // Perform Fisher-Yates shuffle to randomize the order
+  for (let i = randomQuestionArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [randomQuestionArray[i], randomQuestionArray[j]] = [randomQuestionArray[j], randomQuestionArray[i]];
+    }
+    return randomQuestionArray;
+  }
+
+//Beginner Questions
 function loadBeginnerQuestions() {
   const beginnerQuestions = beginnerquestions.filter(question => question.level === 'Beginner');
-  return beginnerQuestions;
+  const randomBeginnerQuestion = randomQuestion(beginnerQuestions);
+  return randomBeginnerQuestion;
 }
 const beginnerQuestions = loadBeginnerQuestions();
 console.log(beginnerQuestions);
 
 
-
+//Intermediate Questions
 function loadIntermediateQuestions() {
   const intermediateQuestions = intermediatequestions.filter(question => question.level === 'Intermediate');
-  return intermediateQuestions;
+  const randomIntermediateQuestion = randomQuestion(intermediateQuestions);
+  return randomIntermediateQuestion;
 }
 const intermediateQuestions = loadIntermediateQuestions();
 console.log(intermediateQuestions);
 
 
+
+//Advanced Questions
 function loadAdvancedQuestions() {
   const advancedQuestions = advancedquestions.filter(question => question.level === 'Advanced');
-  return advancedQuestions;
+  const randomAdvancedQuestion = randomQuestion(advancedQuestions);
+  return randomAdvancedQuestion;
 }
 const advancedQuestions = loadAdvancedQuestions();
 console.log(advancedQuestions);
