8132e2244008ce6d039b24e67e931664bd063ccb
aryanipuspitasari <aryanipuspitasari92@gmail.com>
2023-10-24 11:27:36 +0200

    change the package readline sync to require

--- 01-openinggreeting.js
+++ 01-openinggreeting.js
@@ -1,8 +1,10 @@
 // UI opening
 
 // IMPORT PACKAGE
-import readlineSync from "readline-sync";
+// import readlineSync from "readline-sync";
 import chalk from "chalk";
+import chalkAnimation from 'chalk-animation';
+import inquirer from "inquirer";
 
 // BANNER OPENING FUNCTION
 function bannerOpening() {
@@ -23,6 +25,7 @@ function bannerOpening() {
 
 // TO GET USER NAME FUNCTION
 function getUserName() {
+  /*
   const userName = readlineSync.question(
     chalk.redBright("Welcome to Arnesvex Game! Please enter your name: ")
   );
@@ -32,6 +35,22 @@ function getUserName() {
   } else {
     return "Guest";
   }
+  */
+
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "userName",
+        message: chalk.redBright(
+          "Welcome to Arnesvex Game! Please enter your name: "
+        ),
+        default : "Guest",
+      },
+    ])
+    .then((answers) => {
+      return answers.userName.trim() === "" ? "Guest" : answers.userName ;
+    });
 }
 
 // FUNCTION MAIN MENU
@@ -42,21 +61,49 @@ function mainMenu() {
       "---------------------------------------------------\n          ARNESVEX GAME - Main Menu\n---------------------------------------------------\n\n"
     )
   );
+
+  /*
   console.log(chalk.redBright.bold("Please select your level:"));
   console.log(chalk.bold("1. Beginner"));
   console.log(chalk.bold("2. Intermediate"));
   console.log(chalk.bold("3. Advanced"));
-  console.log(chalk.bold("4. Exit"));
+  console.log(chalk.bold("4. Exit")); 
+
+  */
+
+  const choices = [
+    { name: "Beginner", value: "1" },
+    { name: "Intermediate", value: "2" },
+    { name: "Advanced", value: "3" },
+    { name: "Exit", value: "4" },
+  ];
+
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "level",
+        message: chalk.redBright.bold("Please select your level."),
+        choices: choices,
+      },
+    ])
+    .then((answers) => {
+      handleSelectedLevel(answers.level);
+    }).catch((error) => {
+      console.error("An error occurred:", error);
+    });
+    
 }
 
 // FUNCTION SELECTED LEVEL
 
-function handleSelectedLevel() {
-  mainMenu();
+function handleSelectedLevel(choice) {
+  /*mainMenu();
 
   const choice = readlineSync.question(
     chalk.blueBright(`Please enter the number of your choice`)
   );
+*/
 
   switch (choice) {
     case "1":
@@ -69,7 +116,9 @@ function handleSelectedLevel() {
       // WILL BE FILLED WITH THE FUNCTION OF ADVANCED LEVEL
       break;
     case "4":
-        console.log(chalk.bold.redBright(`
+      console.log(chalk.bold.redBright("\nThank you for playing with us !"));
+      console.log(
+        chalk.bold.redBright(`
         ____               _  
        |  _ \\             | | 
        | |_) |_   _  ___  | | 
@@ -78,16 +127,31 @@ function handleSelectedLevel() {
        |____/ \\__, |\\___| (_) 
                __/ |          
               |___/           
-      `));
+      `)
+      );
       process.exit(0);
-      break;
-    default:
-      console.log(`Please select valid option !`);
-      handleSelectedLevel();
+      
   }
 }
 
 bannerOpening();
+
+getUserName().then((answers) => {
+  const playerName = answers.userName;
+  console.clear();
+  if (playerName) {
+    console.log(chalk.redBright(`Hello, ${playerName}!👋\nLet's get started.\n`));
+  } else {
+    console.log(chalk.redBright(`Hello, Guest!👋\nLet's get started.\n`));
+  }
+
+  mainMenu();
+}).catch((error) => {
+  console.error("An error occurred:", error);
+});
+
+
+/*
 
 const playerName = getUserName();
 
@@ -98,3 +162,4 @@ if (playerName !== null) {
 }
 
 handleSelectedLevel();
+*/
