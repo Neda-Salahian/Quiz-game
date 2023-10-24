984ee08c0621af42d1a9bd8c50264482fa118e99
Neda Salahian <neda.salahian@gmail.com>
2023-10-24 14:57:06 +0200

    create a function of loading question.json

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
-import { readFile } from 'fs/promises';
-import { fileURLToPath } from 'url';
-import path from 'path';
+import beginnerquestions from './data/beginnerquestions.json' assert { type: "json" };
+import intermediatequestions from './data/intermediatequestions.json' assert { type: "json" };
+import advancedquestions from './data/advancedquestions.json' assert { type: "json" };
+// Resource: https://v8.dev/features/import-assertions
+// Tip: assert { type: "json" } 
 
-// 
-async function readQuestions() {
-  try {
-    const currentFileURL = import.meta.url;
-    const currentFilePath = fileURLToPath(currentFileURL);
-    const directoryName = path.dirname(currentFilePath);
-    const filePath = path.join(directoryName, 'data', 'questions.json');
-
-    const data = await readFile(filePath, 'utf8');
-    const questions = JSON.parse(data);
-    console.log(questions);
-  } catch (err) {
-    console.error('Error reading questions file:', err);
-  }
+function loadBeginnerQuestions() {
+  const beginnerQuestions = beginnerquestions.filter(question => question.level === 'Beginner');
+  return beginnerQuestions;
 }
+const beginnerQuestions = loadBeginnerQuestions();
+console.log(beginnerQuestions);
 
-readQuestions();
+
+
+function loadIntermediateQuestions() {
+  const intermediateQuestions = intermediatequestions.filter(question => question.level === 'Intermediate');
+  return intermediateQuestions;
+}
+const intermediateQuestions = loadIntermediateQuestions();
+console.log(intermediateQuestions);
+
+
+function loadAdvancedQuestions() {
+  const advancedQuestions = advancedquestions.filter(question => question.level === 'Advanced');
+  return advancedQuestions;
+}
+const advancedQuestions = loadAdvancedQuestions();
+console.log(advancedQuestions);
+
+
+
+
+// console.log("Beginner Questions:");
+// console.log(beginnerquestions);
+
+// console.log("Intermediate Questions:");
+// console.log(intermediatequestions);
+
+// console.log("Advanced Questions:");
+// console.log(advancedquestions);
+
