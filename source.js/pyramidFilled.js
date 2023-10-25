4db5b0ba0fef29b82fb769a851b8db23a74854ec
Expolicious <b.exposito@icloud.com>
2023-10-25 13:51:16 +0200

    add color to pyramid function, goes wrong

--- source.js/pyramidFilled.js
+++ source.js/pyramidFilled.js
@@ -1,27 +1,51 @@
 import chalk from 'chalk';
+
 const printPyramide = () => {
-    return `
+
+
+const bgBlock = [chalk.bgYellow('     '),chalk.bgRed(`x   x`), , (`     `) ];
+//    bgBlock               [0]                      [1]            [2]            
+
+const result = [true, true, true, false, true, false, true, true, null, null, null, null, null, null, null, null, null, null, null, null, null]
+const blockNumber = ["001", "002", "003", "004","005", "006", "007", "008", "009", "010", "011", "012", "013", "014", "015", "016", "017", "018", "019", "020", "021"];
+for (let i = 0; i = blockNumber.length; i++){
+    const bgBlockNr = [chalk.bgYellow(` ${blockNumber[i]} `), chalk.bgRed(`  X  `), (` ${blockNumber[i]} `)]
+//    bgBlockNr                     [0]                             [1]                         [2]
+
+        if (result[i] === true){
+            bgBlock[0];
+            bgBlockNr[0];
+        } else if (result[i] === null){
+            bgBlock[2];
+            bgBlockNr[2];
+        } else if (result[i] === false) {
+            bgBlock[1];
+            bgBlockNr[1];
+        }
+        return `
                         Pyramide of Arnesvex           
                                                                              <=================> 
                                ______                                        //              //|
-                              |${chalk.bgYellow('      ')}|                                      //              //||
-                              |${chalk.bgYellow('  21  ')}|                                    <=================> ||
-                            __|${chalk.bgYellow('______')}|___                                  ||             ||  ||
-                           |${chalk.bgYellow('      ')}|${chalk.bgYellow('      ')}|                                 ||   Player:   ||  ||
-                           |${chalk.bgYellow('  19  ')}|${chalk.bgYellow('  20  ')}|                                 ||             ||  ||
-                         __|${chalk.bgYellow('______')}|${chalk.bgYellow('______')}|___                              ||             ||  ||
-                        |${chalk.bgYellow('      ')}|${chalk.bgYellow('      ')}|${chalk.bgYellow('      ')}|                             ||  _________  ||  ||
-                        |${chalk.bgYellow('  16  ')}|${chalk.bgYellow('  17  ')}|${chalk.bgYellow('  18  ')}|                             ||             ||  ||
-                     ___|${chalk.bgYellow('______')}|${chalk.bgYellow('______')}|${chalk.bgYellow('______')}|__                           ||   Lives:    ||  ||
-                    |${chalk.bgYellow('      ')}|${chalk.bgYellow('      ')}|${chalk.bgYellow('      ')}|${chalk.bgYellow('      ')}|                          ||             ||  ||
-                    |${chalk.bgYellow('  12  ')}|${chalk.bgYellow('  13  ')}|${chalk.bgYellow('  14  ')}|${chalk.bgYellow('  15  ')}|                          ||             ||  ||
-                  __|${chalk.bgYellow('______')}|${chalk.bgYellow('______')}|${chalk.bgYellow('______')}|${chalk.bgYellow('______')}|___                       ||  _________  ||  ||
-                 |${chalk.bgYellow('      ')}|${chalk.bgYellow('      ')}|${chalk.bgYellow('      ')}|${chalk.bgYellow('      ')}|${chalk.bgYellow('      ')}|                      ||             ||  ||
-                 |${chalk.bgYellow('  07  ')}|${chalk.bgYellow('  08  ')}|${chalk.bgYellow('  09  ')}|${chalk.bgYellow('  10  ')}|${chalk.bgYellow('  11  ')}|                      ||   Score:    ||  ||
-              ___|${chalk.bgYellow('______')}|${chalk.bgYellow('______')}|${chalk.bgYellow('______')}|${chalk.bgYellow('______')}|${chalk.bgYellow('______')}|__                    ||             ||  ||
-             |${chalk.bgYellow('      ')}|${chalk.bgYellow('      ')}|${chalk.bgYellow('      ')}|${chalk.bgYellow('      ')}|${chalk.bgYellow('      ')}|${chalk.bgYellow('      ')}|                   ||             ||  ||
-             |${chalk.bgYellow('  01  ')}|${chalk.bgYellow('  02  ')}|${chalk.bgYellow('  03  ')}|${chalk.bgYellow('  04  ')}|${chalk.bgYellow('  05  ')}|${chalk.bgYellow('  06  ')}|                   ||             || // 
-             |${chalk.bgYellow('      ')}|${chalk.bgYellow('      ')}|${chalk.bgYellow('      ')}|${chalk.bgYellow('      ')}|${chalk.bgYellow('      ')}|${chalk.bgYellow('      ')}|                   ||             ||//    
-                                                                          <=================>
-  `};
+                              |${bgBlock[i]}|                                      //              //||
+                              |${bgBlock[i]}|                                    <=================> ||
+                            __|${bgBlock[i]}|___                                  ||             ||  ||
+                           |${bgBlock[i]}|${bgBlock[i]}|                                 ||   Player:   ||  ||
+                           |${bgBlock[i]}|${bgBlock[i]}|                                 ||             ||  ||
+                         __|${bgBlock[i]}|${bgBlock[i]}|___                              ||             ||  ||
+                        |${bgBlock[i]}|${bgBlock[i]}|${bgBlock[i]}|                             ||  _________  ||  ||
+                        |${bgBlock[i]}|${bgBlock[i]}|${bgBlock[i]}|                             ||             ||  ||
+                     ___|${bgBlock[i]}|${bgBlock[i]}|${bgBlock[i]}|__                           ||   Lives:    ||  ||
+                    |${bgBlock[i]}|${bgBlock[i]}|${bgBlock[i]}|${bgBlock[i]}|                          ||             ||  ||
+                    |${bgBlock[i]}|${bgBlock[i]}|${bgBlock[i]}|${bgBlock[i]}|                          ||             ||  ||
+                  __|${bgBlock[i]}|${bgBlock[i]}|${bgBlock[i]}|${bgBlock[i]}|___                       ||  _________  ||  ||
+                 |${bgBlock[i]}|${bgBlock[i]}|${bgBlock[i]}|${bgBlock[i]}|${bgBlock[i]}|                      ||             ||  ||
+                 |${bgBlock[i]}|${bgBlock[i]}|${bgBlock[i]}|${bgBlock[i]}|${bgBlock[i]}|                      ||   Score:    ||  ||
+              ___|${bgBlock[i]}|${bgBlock[i]}|${bgBlock[i]}|${bgBlock[i]}|${bgBlock[i]}|__                    ||             ||  ||
+             |${bgBlock}|${bgBlock}|${bgBlock}|${bgBlock}|${bgBlock}|${bgBlock}|                   ||             ||  ||
+             |${bgBlockNr}|${bgBlockNr}|${bgBlockNr}|${bgBlockNr}|${bgBlockNr}|${bgBlockNr}|                   ||             || // 
+             |${bgBlock}|${bgBlock}|${bgBlock}|${bgBlock}|${bgBlock}|${bgBlock}|                   ||             ||//    
+                                                                          <=================>                                                                 
+  `}
+};
+
 console.log(printPyramide());
